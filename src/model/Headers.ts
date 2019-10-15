b9d3e512fe21ea631c69ff10c935a4ec5d7aff22
barosanuemailtest <barosanu.email.test@gmail.com>
2019-10-15 17:24:44 +0300

    cors header

--- src/model/Headers.ts
+++ src/model/Headers.ts
@@ -1,12 +1,12 @@
 import * as http from 'http';
 
 export const textHeader: http.OutgoingHttpHeaders = {
-    'Access-Control-Allow-Origin': 'http://localhost:8080',
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'text/plain'
 };
 
 export const jsonHeader: http.OutgoingHttpHeaders = {
-    'Access-Control-Allow-Origin': 'http://localhost:8080',
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json'
 };
 
