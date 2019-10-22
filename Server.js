e1bd2e802dcecf9e5d9488c4c236c3598079f6a1
barosanuemailtest <barosanu.email.test@gmail.com>
2019-10-22 14:03:39 +0300

    FIx lint in Server.js

--- Server.js
+++ Server.js
@@ -1,33 +1,34 @@
+/* eslint-disable no-var */
+/* eslint-disable no-use-before-define */
 var http = require('http');
 
 const manager = {
     name: 'bos',
     age: 37
-}
+};
 
 const empl1 = {
     name: 'John Popescu',
     age: 22
-}
+};
 
 const empl2 = {
     name: 'Doe Vasile',
     age: 34
-}
+};
 
-const corsHeader = { 'Access-Control-Allow-Origin': '*' }
-const textHeader = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/plain', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' }
-const jsonHeader = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' , 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}
+const textHeader = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/plain', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' };
+const jsonHeader = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' };
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
 
-    console.log('called page!')
+    console.log('called page!');
     const url = req.url;
     switch (url) {
         case '/managers':
             res.writeHead(200, jsonHeader);
             res.end(JSON.stringify(manager));
-            console.log(manager)
+            console.log(manager);
             break;
         case '/employees':
             writeHead(res);
@@ -43,7 +44,7 @@ http.createServer(function (req, res) {
 
 /**
  * Writes the response
- * @param {*} res 
+ * @param {*} res
  */
 function writeHead(res) {
     res.header('Access-Control-Allow-Origin', '*');
